ce55826131cb69f9588a70c9f27fd33d7fb050d4
1NS <ins16mail@gmail.com>
2024-02-15 14:40:55 +0300

    Update AbsoluteFix.lua
    - minor fixes

--- luascripts/absolutefix/moonloader/AbsoluteFix.lua
+++ luascripts/absolutefix/moonloader/AbsoluteFix.lua
@@ -87,7 +87,7 @@ local hostip = "193.84.90.23"
 local isAbsoluteRoleplay = false
 local dialogs = {}
 local removed_objects = {647, 1410, 1412, 1413} 
-local restored_objects = {3337, 3244, 3276, 1290} 
+local restored_objects = {3337, 3244, 3276, 1290, 1540} 
 local attached_objects = {}
 local isPlayerSpectating = false
 local dialogRestoreText = false
@@ -106,7 +106,9 @@ function imgui.OnDrawFrame()
      
       imgui.Begin(u8"AbsoluteFix", dialog.settings)
        
-      imgui.TextColoredRGB("Набор исправлений и дополнений для серверов {007DFF}Absolute Play")
+      imgui.Text(u8"Набор исправлений и дополнений для серверов")
+      imgui.SameLine()
+      imgui.TextColored(imgui.ImVec4(0, 0.49, 1, 1.0), u8"Absolute Play")
 	  if imgui.IsItemClicked() then
          setClipboardText("gta-samp.ru")
          printStringNow("Url copied to clipboard", 1000)
@@ -650,7 +652,11 @@ function sampev.onServerMessage(color, text)
       if text:find("Рекорд игроков на сервере") then
          return false
       end
-
+      
+      if text:find("Ты слишком далеко от транспорта") then
+         return false
+      end
+      
       if text:find("Вконтакте") then
          -- Клавиша Y - Настройки - Аккаунт - Вконтакте
          -- Рекомендуется прикрепить страницу Вконтакте для защиты аккаунта
@@ -840,58 +846,6 @@ function onSendRpc(id, bs, priority, reliability, channel, shiftTimestamp)
 end
 
 -- Imgui extended functions
-function imgui.TextColoredRGB(text)
-    local style = imgui.GetStyle()
-    local colors = style.Colors
-    local ImVec4 = imgui.ImVec4
-
-    local explode_argb = function(argb)
-        local a = bit.band(bit.rshift(argb, 24), 0xFF)
-        local r = bit.band(bit.rshift(argb, 16), 0xFF)
-        local g = bit.band(bit.rshift(argb, 8), 0xFF)
-        local b = bit.band(argb, 0xFF)
-        return a, r, g, b
-    end
-
-    local getcolor = function(color)
-        if color:sub(1, 6):upper() == 'SSSSSS' then
-            local r, g, b = colors[1].x, colors[1].y, colors[1].z
-            local a = tonumber(color:sub(7, 8), 16) or colors[1].w * 255
-            return ImVec4(r, g, b, a / 255)
-        end
-        local color = type(color) == 'string' and tonumber(color, 16) or color
-        if type(color) ~= 'number' then return end
-        local r, g, b, a = explode_argb(color)
-        return imgui.ImColor(r, g, b, a):GetVec4()
-    end
-
-    local render_text = function(text_)
-        for w in text_:gmatch('[^\r\n]+') do
-            local text, colors_, m = {}, {}, 1
-            w = w:gsub('{(......)}', '{%1FF}')
-            while w:find('{........}') do
-                local n, k = w:find('{........}')
-                local color = getcolor(w:sub(n + 1, k - 1))
-                if color then
-                    text[#text], text[#text + 1] = w:sub(m, n - 1), w:sub(k + 1, #w)
-                    colors_[#colors_ + 1] = color
-                    m = n
-                end
-                w = w:sub(1, n - 1) .. w:sub(k + 1, #w)
-            end
-            if text[0] then
-                for i = 0, #text do
-                    imgui.TextColored(colors_[i] or colors[1], u8(text[i]))
-                    imgui.SameLine(nil, 0)
-                end
-                imgui.NewLine()
-            else imgui.Text(u8(w)) end
-        end
-    end
-
-    render_text(text)
-end
-
 function imgui.TextQuestion(label, description)
     imgui.TextDisabled(label)
 
